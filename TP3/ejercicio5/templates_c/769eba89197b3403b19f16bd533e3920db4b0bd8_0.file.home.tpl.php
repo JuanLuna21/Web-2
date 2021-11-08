e0c03d6eab1c11b3c3bfce5f78a936d877d968cd
Juan Luna <xjuanigancio@gmail.com>
2021-11-08 19:53:17 -0300

    add base url

--- TP3/ejercicio5/templates_c/769eba89197b3403b19f16bd533e3920db4b0bd8_0.file.home.tpl.php
+++ TP3/ejercicio5/templates_c/769eba89197b3403b19f16bd533e3920db4b0bd8_0.file.home.tpl.php
@@ -1,18 +1,18 @@
 <?php
-/* Smarty version 3.1.39, created on 2021-11-08 22:57:16
+/* Smarty version 3.1.39, created on 2021-11-08 23:51:06
   from 'C:\xampp\htdocs\Practice\TP3\ejercicio5\templates\home.tpl' */
 
 /* @var Smarty_Internal_Template $_smarty_tpl */
 if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   'version' => '3.1.39',
-  'unifunc' => 'content_61899d3cb03eb9_91622868',
+  'unifunc' => 'content_6189a9da3a6cf9_45452725',
   'has_nocache_code' => false,
   'file_dependency' => 
   array (
     '769eba89197b3403b19f16bd533e3920db4b0bd8' => 
     array (
       0 => 'C:\\xampp\\htdocs\\Practice\\TP3\\ejercicio5\\templates\\home.tpl',
-      1 => 1636408635,
+      1 => 1636411847,
       2 => 'file',
     ),
   ),
@@ -20,7 +20,7 @@ if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   array (
   ),
 ),false)) {
-function content_61899d3cb03eb9_91622868 (Smarty_Internal_Template $_smarty_tpl) {
+function content_6189a9da3a6cf9_45452725 (Smarty_Internal_Template $_smarty_tpl) {
 ?><!DOCTYPE html>
 <html lang="en">
 <head>
@@ -53,7 +53,8 @@ $_smarty_tpl->tpl_vars['materia']->do_else = false;
 </td>
             <td><?php echo $_smarty_tpl->tpl_vars['materia']->value->profesor;?>
 </td>
-            <td><a href="editarMaterias/<?php echo $_smarty_tpl->tpl_vars['materia']->value->id_materia;?>
+            <td><a href="<?php echo BASE_URL;?>
+editarMaterias/<?php echo $_smarty_tpl->tpl_vars['materia']->value->id_materia;?>
 ">Editar</a></td>
         </tr>
     </tbody>
